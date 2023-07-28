b82390f3773e482a2cf2b406ca697faf45d2c09b
jieunryu382 <jieunryu382@gmail.com>
2023-07-28 23:50:42 +0900

    animation toggle

--- JS/section_toggle.js
+++ JS/section_toggle.js
@@ -7,12 +7,17 @@ const sWidth = document.querySelectorAll(".section_width");
 
 for (let i = 0; i < sButton.length; i++) {
   sButton[i].addEventListener("click", () => {
-    sContents[i].classList.toggle("hidden");
     sOpen[i].classList.toggle("hidden");
     sClose[i].classList.toggle("hidden");
     if (sWidth[i].clientWidth == 300) {
+      sContents[i].classList.toggle("hidden");
+      sWidth[i].style.animation = "closeSection 0.4s";
       sWidth[i].style.minWidth = "70px";
     } else {
+      setTimeout(() => {
+        sContents[i].classList.toggle("hidden");
+      }, 300);
+      sWidth[i].style.animation = "openSection 0.4s";
       sWidth[i].style.minWidth = "300px";
     }
   });
