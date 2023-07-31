28582eaf808b93c4ce94c3ddbd3986b2c9b32939
jieunryu382 <jieunryu382@gmail.com>
2023-07-31 23:56:44 +0900

    popup input

--- JS/popup_input.js
+++ JS/popup_input.js
@@ -1,9 +1,10 @@
 const title = document.querySelector("#input_title");
 const description = document.querySelector("#input_description");
-const period = document.querySelector("#input_period");
+const periodNumber = document.querySelector("#input_period");
 const startDay = document.querySelector("#input_start");
 const endDay = document.querySelector("#input_end");
 const submit = document.querySelector("#popup_submit");
+const form = document.querySelector("#new_note_popup");
 
 const today = new Date();
 const endDate = new Date(today);
@@ -12,6 +13,8 @@ endDate.setDate(today.getDate() + 3);
 
 startDay.value = today.toISOString().substring(0, 10);
 endDay.value = endDate.toISOString().substring(0, 10);
+startDay.setAttribute("max", endDay.value);
+endDay.setAttribute("min", startDay.value);
 
 startDay.addEventListener("change", intervalDay);
 endDay.addEventListener("change", intervalDay);
@@ -20,5 +23,23 @@ function intervalDay() {
   const interval =
     new Date(endDay.value).getTime() - new Date(startDay.value).getTime();
   const intervalDay = interval / (1000 * 60 * 60 * 24);
-  period.innerHTML = intervalDay.toString() + "일동안";
+  if (intervalDay == 0) {
+    periodNumber.innerHTML = "당일치기";
+  } else {
+    periodNumber.innerHTML = intervalDay.toString() + "일동안";
+  }
 }
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const noteTitle = title.value;
+  const noteDes = description.value;
+  const period = [startDay.value, endDay.value, periodNumber.innerHTML];
+  if (noteTitle == "" || noteDes == "") {
+    alert("내용을 입력하세요.");
+  } else {
+    console.log("제목:", noteTitle);
+    console.log("설명:", noteDes);
+    console.log("기간:", period);
+  }
+});
